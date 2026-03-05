93fe98cac5b762898c7851842ed2086844fbb3fe
fjr <fjrkazmi@gmail.com>
2026-03-05 08:31:55 +0500

    adding app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import os
-
-if os.environ.get('RENDER'):
-    app.config['DEBUG'] = False
-else:
-    app.config['DEBUG'] = True
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import os
 import torch
@@ -192,4 +186,4 @@ def not_found(error):
 
 # ====== Run Application ======
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
